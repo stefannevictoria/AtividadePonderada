de03e98b125da12267d9b5c3c021c83cd5af8cd9
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-02-11 13:27:36 -0300

    Exercício Média

--- MediaNotas.js
+++ MediaNotas.js
@@ -1,7 +1,32 @@
-// Armazenando a nota do aluno
-var nota1 = 13; 
-var nota2 = 9; 
-var nota3 = 7; 
+// Armazenando as notas do aluno
+var notas = [4, 8, 9];
+
+// Definindo a média mínima para aprovação
+var mediaMinima = 7; 
+
+// Soma das notas
+var soma = 0;
+
+// Índice inicial para o loop
+var i = 0;
+
+while (i < notas.length) {
+    soma += notas[i]; // Adiciona a nota atual à soma
+    i++; // Incrementa o índice 
+}
+
+// Calculando a média das notas do aluno
+var media = soma / notas.length;
+
+// Verificando se o aluno foi aprovado
+if (media >= mediaMinima && media <= 10) {
+    // Exibindo o resultado
+    console.log ('Parabéns!! Você foi aprovado! Sua média foi:' + media.toFixed(2));
+} else {
+    // Exibindo o resultado
+    console.log ('Não foi dessa vez :(. Você foi reprovado! Sua média foi:' + media.toFixed(2));
+}
+
 
 // Verificando se as notas estão dentro do intervalo válido
 for (var i = 0; i < 1; i++) {
@@ -9,19 +34,4 @@ for (var i = 0; i < 1; i++) {
         console.log('Valor incompatível. Por favor, insira notas válidas entre 0 e 10.');
         return;
     }
-}
-
-// Calculando a média do aluno
-var media = (nota1 + nota2 + nota3) / 3; 
-
-// Definindo a média mínima para aprovação
-var mediaMinima = 7; 
-
-// Verificando se o aluno foi aprovado
-if (media >= mediaMinima) {
-    // Exibindo o resultado
-    console.log ('Parabéns!! Você foi aprovado! Sua média foi:' + media.toFixed(2));
-} else {
-    // Exibindo o resultado
-    console.log ('Não foi dessa vez :(. Você foi reprovado! Sua média foi:' + media.toFixed(2));
 }
