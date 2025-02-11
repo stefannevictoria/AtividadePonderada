b2c4aa688b9dece95e19b0a7557caa7da5ca29d6
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-02-11 13:14:19 -0300

    Create MediaNotas.js

---
+++ MediaNotas.js
@@ -0,0 +1,27 @@
+// Armazenando a nota do aluno
+var nota1 = 13; 
+var nota2 = 9; 
+var nota3 = 7; 
+
+// Verificando se as notas estão dentro do intervalo válido
+for (var i = 0; i < 1; i++) {
+    if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10) {
+        console.log('Valor incompatível. Por favor, insira notas válidas entre 0 e 10.');
+        return;
+    }
+}
+
+// Calculando a média do aluno
+var media = (nota1 + nota2 + nota3) / 3; 
+
+// Definindo a média mínima para aprovação
+var mediaMinima = 7; 
+
+// Verificando se o aluno foi aprovado
+if (media >= mediaMinima) {
+    // Exibindo o resultado
+    console.log ('Parabéns!! Você foi aprovado! Sua média foi:' + media.toFixed(2));
+} else {
+    // Exibindo o resultado
+    console.log ('Não foi dessa vez :(. Você foi reprovado! Sua média foi:' + media.toFixed(2));
+}
