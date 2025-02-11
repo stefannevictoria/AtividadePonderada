943b78a319901a9f8c1091bca8ecf58724a691ae
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-02-11 11:34:26 -0300

    Create atividade.js

---
+++ atividade.js
@@ -0,0 +1,25 @@
+// Armazenando a nota do aluno
+var nota1 = 6; 
+var nota2 = 9; 
+var nota3 = 7; 
+
+// Verificando se as notas estão dentro do intervalo válido
+while (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10) {
+    console.log('Valor incompatível. Por favor, insira notas válidas entre 0 e 10.');
+    break;
+}
+
+// Calculando a média do aluno
+var media = (nota1 + nota2 + nota3) / 3; 
+
+// Definindo a média mínima para aprovação
+var mediaMinima = 7; 
+
+// Verificando se o aluno foi aprovado
+if (media >= mediaMinima) {
+    // Exibindo o resultado
+    console.log ('Parabéns!! Você foi aprovado! Sua média foi:' + media);
+} else {
+    // Exibindo o resultado
+    console.log ('Não foi dessa vez :(. Você foi reprovado! Sua média foi:' + media);
+}
